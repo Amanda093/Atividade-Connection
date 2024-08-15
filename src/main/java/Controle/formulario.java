eea998bef33fe60543fc4cf678489c2394b69765
Amanda093 <vovigadagamer@gmail.com>
2024-08-15 20:54:59 -0300

    commit

--- src/main/java/Controle/formulario.java
+++ src/main/java/Controle/formulario.java
@@ -1,26 +1,28 @@
 package Controle;
 
-// Parei na criação de barra de pesquisa
 import java.awt.*;
 import java.text.*;
 import javax.swing.*;
+import java.awt.event.*;
+import java.util.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.text.MaskFormatter;
 
 import Conexao.conexao;
+import java.awt.event.KeyEvent;
 import javax.swing.table.DefaultTableModel;
 import java.sql.*;
 
 public class formulario extends JFrame {
     conexao con_cliente; // banco de dados
     
-    JLabel rCod, rNome, rEmail, rTel, rData;
-    JTextField tCod, tNome, tEmail;
+    JLabel rTitulo, rCod, rNome, rEmail, rTel, rData, rPesquisa;
+    JTextField tCod, tNome, tEmail, tPesquisa;
     JFormattedTextField tTel, tData;
     MaskFormatter mTel, mData;
     
-    JButton bFirst, bLast, bPrevious, bNext, bLimpar, bGravar, bAlterar, bExcluir; // dps adicionar na pagina
+    JButton bFirst, bLast, bPrevious, bNext, bLimpar, bGravar, bAlterar, bExcluir, bPesquisa;
     ImageIcon imagens[]; // imagens dos botões
     
     JTable tb_clientes; // datagrid
@@ -30,18 +32,21 @@ public class formulario extends JFrame {
         con_cliente = new conexao(); // inicialização do objeto
         con_cliente.conecta(); // chama o método que conecta
         
+        setTitle("Conexão Java com MySql"); // define o titulo da janela
         Container tela = getContentPane();
-        setTitle("Conexão Java com MySql");
         tela.setLayout(null);
         setResizable(false);
-
+        
          // icon da página
-        ImageIcon icone = new ImageIcon("");
+        ImageIcon icone = new ImageIcon("image/form.png");
         setIconImage(icone.getImage());
         
         // icones dos botões
-        String icones[]={"image/next.png"};
-        
+        String icones[]={"image/editar.png","image/excluir.png","image/filtro.png","image/pesquisa.png","image/salva.png"};
+        imagens = new ImageIcon[5];
+        for(int i = 0; i < 5; i++){
+            imagens[i] = new ImageIcon(icones[i]);
+        }
         
         // máscara da data e do telefone
         try {
@@ -52,25 +57,33 @@ public class formulario extends JFrame {
         }
         catch (ParseException erro){}
         
-        rCod = new JLabel("Codigo: "); 
+        // nomeação dos conteudos da tela
+        rTitulo = new JLabel("FORMULÁRIO");
+        rTitulo.setFont(new Font("Segoe UI",Font.BOLD,24));
+        
+        rCod = new JLabel("Código: "); 
         rNome = new JLabel("Nome: "); 
         rEmail = new JLabel("Email: "); 
         rTel = new JLabel("Telefone: "); 
         rData = new JLabel("Data: ");
+        rPesquisa = new JLabel("Pesquisar pelo nome do Cliente: ");
         
         bFirst = new JButton("Primeiro");
-        bLast = new JButton("Último");
+        bLast = new JButton("Ultimo");
         bPrevious = new JButton("Anterior");
         bNext = new JButton("Próximo");
         
-        bLimpar = new JButton("Limpar");
+        bLimpar = new JButton(imagens[0]);
         bGravar = new JButton("Gravar");
         bAlterar = new JButton("Alterar");
         bExcluir = new JButton("Excluir");
+        bPesquisa = new JButton("Pesquisar"); 
         
         tCod = new JTextField("");
         tNome = new JTextField("");
         tEmail = new JTextField("");
+        tPesquisa = new JTextField("");
+        
         tTel = new JFormattedTextField(mTel);
         tData = new JFormattedTextField(mData);
         
@@ -78,70 +91,44 @@ public class formulario extends JFrame {
         scroll_tb = new javax.swing.JScrollPane();
 
         // setando a posição dos conteúdos na janela
-        rCod.setBounds(50,50,50,20);        tCod.setBounds(120,50,200,20);
-        rNome.setBounds(50,90,100,20);      tNome.setBounds(120,90,80,20);
-        rEmail.setBounds(50,130,300,20);    tEmail.setBounds(120,130,300,20);
-        rTel.setBounds(50,170,300,20);      tTel.setBounds(120,170,300,20);
-        rData.setBounds(50,210,300,20);     tData.setBounds(120,210,300,20);
+        rTitulo.setBounds(320,30,500,20);
+        rCod.setBounds(50,70,50,20);        tCod.setBounds(120,70,50,20);
+        rNome.setBounds(50,110,100,20);     tNome.setBounds(120,110,200,20);
+        rEmail.setBounds(50,150,300,20);    tEmail.setBounds(120,150,200,20);
+        rTel.setBounds(50,190,300,20);      tTel.setBounds(120,190,90,20);
+        rData.setBounds(50,230,300,20);     tData.setBounds(120,230,70,20);
         
+        bFirst.setBounds(50,270,100,25);     bLast.setBounds(150,270,100,25);
+        bPrevious.setBounds(250,270,100,25); bNext.setBounds(350,270,100,25);
+        bLimpar.setBounds(350,70,100,30);    bGravar.setBounds(200,50,200,20);
+        bAlterar.setBounds(400,50,200,20);   bExcluir.setBounds(200,50,200,20);
+        
+        tela.add(rTitulo);   
         tela.add(rCod);      tela.add(tCod);
         tela.add(rNome);     tela.add(tNome);
         tela.add(rEmail);    tela.add(tEmail);
         tela.add(rTel);      tela.add(tTel);
         tela.add(rData);     tela.add(tData);
         
+        tela.add(bFirst);    tela.add(bLast);
+        tela.add(bPrevious); tela.add(bNext);
+        tela.add(bLimpar);   tela.add(bGravar);
+        tela.add(bAlterar);  tela.add(bExcluir);
+        
         // começo das funcionalidade dos JButton
-        tipo_Botao("First");
-        bLast.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    con_cliente.resultset.last();
-                    mostrar_Dados();
-                } catch (SQLException erro) {
-                    JOptionPane.showMessageDialog(null, "Não foi possível acessar o último registro"
-                            + erro, "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
-                }
-            }
-        });
-        bPrevious.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    con_cliente.resultset.previous();
-                    mostrar_Dados();
-                } catch (SQLException erro) {
-                    JOptionPane.showMessageDialog(null, "Não foi possível acessar o Anterior registro"
-                            + erro, "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
-                }
-            }
-        });
-        bNext.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    con_cliente.resultset.next();
-                    mostrar_Dados();
-                } catch (SQLException erro) {
-                    JOptionPane.showMessageDialog(null, "Não foi possível acessar o Próximo registro"
-                            + erro, "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
-                }
-            }
-        });
+        tipo_Botao(bFirst);    tipo_Botao(bLast);
+        tipo_Botao(bPrevious); tipo_Botao(bNext);
+        
         bLimpar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                tCod.setText("");
-                tNome.setText("");
-                tEmail.setText("");
-                tTel.setText("");
-                tData.setText("");
-                
-                tCod.requestFocus(); // posiciona o cursor neste campo
-            }
+                tCod.setText("");   tNome.setText("");
+                tEmail.setText(""); tTel.setText("");
+                tData.setText("");  tCod.requestFocus(); }
         });
         bGravar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                String nome = tNome.getText();
-                String email = tEmail.getText();
-                String telefone = tTel.getText();
-                String data_nasc = tData.getText();
+                String nome = tNome.getText();      String email = tEmail.getText();
+                String telefone = tTel.getText();   String data_nasc = tData.getText();
                 
                 try {
                     String insert_sql = "insert into tbclientes (nome, telefone, email, dt_nasc) values ('" + nome + "','" + telefone + "','" + email + "','" + data_nasc + "')";
@@ -153,16 +140,13 @@ public class formulario extends JFrame {
                     preencherTabela();
                 } catch (SQLException errosql) {
                     JOptionPane.showMessageDialog(null, "\n Erro na Gravação: \n " 
-                            + errosql, "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
-                }
+                            + errosql, "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE); }
             }
         });
         bAlterar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                String nome = tNome.getText();
-                String email = tEmail.getText();
-                String telefone = tTel.getText();
-                String data_nasc = tData.getText();
+                String nome = tNome.getText();      String email = tEmail.getText();
+                String telefone = tTel.getText();   String data_nasc = tData.getText();
                 String sql; String msg = "";
                 
                 try {
@@ -215,8 +199,8 @@ public class formulario extends JFrame {
         // fim das funcionalidade dos JButton
         
         // começo da configuração da JTable
-        tb_clientes.setBounds(50, 250, 550, 200);
-        scroll_tb.setBounds(50, 250, 550, 200);
+        tb_clientes.setBounds(50,315,550,200);
+        scroll_tb.setBounds(50,315,550,200);
         
         tela.add(tb_clientes);
         tela.add(scroll_tb);
@@ -249,8 +233,21 @@ public class formulario extends JFrame {
         scroll_tb.setViewportView(tb_clientes);
         tb_clientes.setAutoCreateRowSorter(true);
         // fim da configuração da JTable
-                
-        setSize(800, 600);
+           
+        // dicas
+        bFirst.setToolTipText("Pressione ALT + P para executar a ação deste botão");
+        bLast.setToolTipText("Pressione ALT + U para executar a ação deste botão");
+        bPrevious.setToolTipText("Pressione ALT + A para executar a ação deste botão");
+        bNext.setToolTipText("Pressione ALT + R para executar a ação deste botão");
+        
+        // atalhos
+        getRootPane().setDefaultButton(bLimpar);
+        bFirst.setMnemonic(KeyEvent.VK_P);
+        bLast.setMnemonic(KeyEvent.VK_U);
+        bPrevious.setMnemonic(KeyEvent.VK_A);
+        bNext.setMnemonic(KeyEvent.VK_R);
+        
+        setSize(800,600);
         setVisible(true);
         setLocationRelativeTo(null);
         
@@ -258,30 +255,29 @@ public class formulario extends JFrame {
         preencherTabela();
         posicionarRegistro();
     }
-    public JButton tipo_Botao(String nome) {
-        JButton botao = new JButton(nome);
-        botao.addActionListener(new ActionListener() {
+    public void tipo_Botao(JButton nome) {
+        nome.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 try {
-                    if (nome.equals("Primeiro")) {
-                        con_cliente.resultset.first();
-                        mostrar_Dados(); } 
-                    else if (nome.equals("Primeiro")) {
-                        con_cliente.resultset.first();
-                        mostrar_Dados(); }
-                    else if (nome.equals("Primeiro")) {
-                        con_cliente.resultset.first();
-                        mostrar_Dados(); }
-                    else if (nome.equals("Primeiro")) {
-                        con_cliente.resultset.first();
-                        mostrar_Dados(); }
+                    if (nome.equals(bFirst)) {
+                        con_cliente.resultset.first(); } 
+                    
+                    else if (nome.equals(bLast)) {
+                        con_cliente.resultset.last(); }
+                    
+                    else if (nome.equals(bPrevious)) {
+                        con_cliente.resultset.previous(); }
+                    
+                    else if (nome.equals(bNext)) {
+                        con_cliente.resultset.next(); }
+                    
+                    mostrar_Dados(); // apresenta os dados
                 } catch (SQLException erro) {
                     JOptionPane.showMessageDialog(null, "Não foi possível acessar o primeiro registro"
                             + erro, "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
                 }  
             }
         });
-        return botao;
     }
     public void posicionarRegistro() {
         try {
@@ -305,11 +301,11 @@ public class formulario extends JFrame {
         }
     }
     public void preencherTabela() {
-        tb_clientes.getColumnModel().getColumn(0).setPreferredWidth(4);
+        tb_clientes.getColumnModel().getColumn(0).setPreferredWidth(50);
         tb_clientes.getColumnModel().getColumn(1).setPreferredWidth(150);
-        tb_clientes.getColumnModel().getColumn(2).setPreferredWidth(11);
-        tb_clientes.getColumnModel().getColumn(3).setPreferredWidth(14);
-        tb_clientes.getColumnModel().getColumn(4).setPreferredWidth(100);
+        tb_clientes.getColumnModel().getColumn(2).setPreferredWidth(100);
+        tb_clientes.getColumnModel().getColumn(3).setPreferredWidth(90);
+        tb_clientes.getColumnModel().getColumn(4).setPreferredWidth(150);
         
         DefaultTableModel modelo = (DefaultTableModel) tb_clientes.getModel();
         modelo.setNumRows(0);
@@ -328,7 +324,7 @@ public class formulario extends JFrame {
         } catch (SQLException erro) {
             JOptionPane.showMessageDialog(null, "\n Erro ao listar dados da tabela!! :\n " + erro,
                     "Mensagem do Programa", JOptionPane.INFORMATION_MESSAGE);
-        } // PAREI AQUI!!
+        }
     }
     public static void main(String args[]){
         formulario app = new formulario();
